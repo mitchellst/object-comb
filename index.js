9f7738ce64981f09015d50bc9d7f8d863ac477fa
Mitchell Stoutin <mitchell.stoutin@gmail.com>
2016-09-08 10:40:33 -0500

    smaller

--- index.js
+++ index.js
@@ -8,20 +8,11 @@ module.exports = objectComb;
  * @returns {any} a transformed version of obj.
  */
 function objectComb(obj, operation, test=_.isString){
-  if (test(obj)) {
-    obj = operation(obj); 
-  }
+  obj = test(obj) ? operation(obj) : obj;
 
   if(_.isArray(obj) || _.isObject(obj)){
     let mapFunction = _.isArray(obj) ? _.map : _.mapValues;
-    
-    return mapFunction(obj, val => {
-      if(test(val)){
-        return operation(val);
-      } else if (_.isObject(val) || _.isArray(val)) {
-        return objectComb(val, operation, test); 
-      } else return val;
-    });
+    return mapFunction(obj, val => objectComb(val, operation, test));
   }
   
   return obj;
